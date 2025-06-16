ef04fbd214c32558e123253b77294ce77dbcc244
dtuchs <dtuchs@gmail.com>
2025-06-16 23:47:58 +0300

    Video #3.1 [20.06.25] - Intro to JUnit Extensions. BrowserExtension, interface SuiteExtension

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CreateSpendingExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CreateSpendingExtension.java
@@ -13,9 +13,9 @@ import org.junit.platform.commons.support.AnnotationSupport;
 
 import java.util.Date;
 
-public class SpendingExtension implements BeforeEachCallback, ParameterResolver {
+public class CreateSpendingExtension implements BeforeEachCallback {
 
-  public static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(SpendingExtension.class);
+  public static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(CreateSpendingExtension.class);
   private final SpendApiClient spendApiClient = new SpendApiClient();
 
   @Override
@@ -45,15 +45,5 @@ public class SpendingExtension implements BeforeEachCallback, ParameterResolver 
           );
         }
     );
-  }
-
-  @Override
-  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-    return parameterContext.getParameter().getType().isAssignableFrom(SpendJson.class);
-  }
-
-  @Override
-  public SpendJson resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-    return extensionContext.getStore(NAMESPACE).get(extensionContext.getUniqueId(), SpendJson.class);
   }
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/LoginTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/LoginTest.java
@@ -2,9 +2,12 @@ package guru.qa.niffler.test;
 
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.BrowserExtension;
 import guru.qa.niffler.page.LoginPage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith(BrowserExtension.class)
 public class LoginTest {
 
   private static final Config CFG = Config.getInstance();
